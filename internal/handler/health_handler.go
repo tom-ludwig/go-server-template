32e93a2d1d91325639b0d2cabb6a89bd75a01721
Tom Ludwig <tommludwig@icloud.com>
2025-12-23 15:08:21 +0100

    feat: Split openapi files for health and user routes

--- internal/handler/health_handler.go
+++ internal/handler/health_handler.go
@@ -3,38 +3,47 @@ package handler
 import (
 	"context"
 
-	"com.tom-ludwig/go-server-template/internal/api"
+	"com.tom-ludwig/go-server-template/internal/api/health"
 	"com.tom-ludwig/go-server-template/internal/repository"
 )
+
+// compile-time check
+var _ health.StrictServerInterface = (*HealthHandler)(nil)
 
 type HealthHandler struct {
 	queries *repository.Queries
 }
 
-func (s *HealthHandler) GetHealthz(ctx context.Context, request api.GetHealthzRequestObject) (api.GetHealthzResponseObject, error) {
-	return api.GetHealthz200JSONResponse{
+func NewHealthHandler(queries *repository.Queries) *HealthHandler {
+	return &HealthHandler{
+		queries: queries,
+	}
+}
+
+func (s *HealthHandler) GetHealthz(ctx context.Context, request health.GetHealthzRequestObject) (health.GetHealthzResponseObject, error) {
+	return health.GetHealthz200JSONResponse{
 		Status: "OK",
 	}, nil
 }
 
-// GetLivez implements api.StrictServerInterface.
-func (s *HealthHandler) GetLivez(ctx context.Context, request api.GetLivezRequestObject) (api.GetLivezResponseObject, error) {
-	return api.GetLivez200JSONResponse{
+// GetLivez implements health.StrictServerInterface.
+func (s *HealthHandler) GetLivez(ctx context.Context, request health.GetLivezRequestObject) (health.GetLivezResponseObject, error) {
+	return health.GetLivez200JSONResponse{
 		Status: "OK",
 	}, nil
 }
 
-// GetReadyz implements api.StrictServerInterface.
-func (s *HealthHandler) GetReadyz(ctx context.Context, request api.GetReadyzRequestObject) (api.GetReadyzResponseObject, error) {
+// GetReadyz implements health.StrictServerInterface.
+func (s *HealthHandler) GetReadyz(ctx context.Context, request health.GetReadyzRequestObject) (health.GetReadyzResponseObject, error) {
 	_, err := s.queries.Ping(ctx)
 	if err != nil {
-		return api.GetReadyz503JSONResponse{
+		return health.GetReadyz503JSONResponse{
 			FailedChecks:      []string{"Database not reachable."},
 			SuccessfullChecks: []string{},
 		}, nil
 	}
 
-	return api.GetReadyz200JSONResponse{
+	return health.GetReadyz200JSONResponse{
 		Status: "OK",
 	}, nil
 }
--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -5,38 +5,40 @@ import (
 	"database/sql"
 	"errors"
 
-	"com.tom-ludwig/go-server-template/internal/api"
+	"com.tom-ludwig/go-server-template/internal/api/users"
 	"com.tom-ludwig/go-server-template/internal/repository"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
+// compile-time check
+var _ users.StrictServerInterface = (*UserHandler)(nil)
+
 type UserHandler struct {
 	Queries *repository.Queries
 }
 
-func (u *UserHandler) GetUser(ctx context.Context, request api.GetUserRequestObject) (api.GetUserResponseObject, error) {
+func NewUserHandler(queries *repository.Queries) *UserHandler {
+	return &UserHandler{
+		Queries: queries,
+	}
+}
+
+func (u *UserHandler) GetUser(ctx context.Context, request users.GetUserRequestObject) (users.GetUserResponseObject, error) {
 	userUUID, err := uuid.Parse(request.Params.UserId)
 	if err != nil {
-		return api.GetUser400JSONResponse{
-			Message: "Invalid user ID",
-		}, nil
+		return users.GetUser400JSONResponse{}, nil
 	}
 	user, err := u.Queries.GetUser(ctx, userUUID)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return api.GetUser404JSONResponse{Message: "User not found"}, nil
+			return users.GetUser404JSONResponse{}, nil
 		}
 
-		return api.GetUser404JSONResponse{Message: "Error 500"}, nil
-		// TODO: Return 500
-		// return api.{
-		// 	InternalServerErrrorJSONResponse: api.InternalServerErrrorJSONResponse{
-		// 		Message: "Failed to get user",
-		// 	},
-		// }, nil
+		return users.GetUser404JSONResponse{}, nil
+		// return users.GetUser500JSONResponse{}, nil
 	}
-	return api.GetUser200JSONResponse{
+	return users.GetUser200JSONResponse{
 		UserId:    user.UserID.String(),
 		FirstName: user.FirstName,
 		LastName:  user.LastName,
@@ -44,7 +46,7 @@ func (u *UserHandler) GetUser(ctx context.Context, request api.GetUserRequestObj
 	}, nil
 }
 
-func (u *UserHandler) CreateUser(ctx context.Context, request api.CreateUserRequestObject) (api.CreateUserResponseObject, error) {
+func (u *UserHandler) CreateUser(ctx context.Context, request users.CreateUserRequestObject) (users.CreateUserResponseObject, error) {
 	newUser, err := u.Queries.CreateUser(ctx, repository.CreateUserParams{
 		FirstName: request.Body.FirstName,
 		LastName:  request.Body.LastName,
@@ -52,14 +54,10 @@ func (u *UserHandler) CreateUser(ctx context.Context, request api.CreateUserRequ
 	})
 
 	if err != nil {
-		return api.CreateUser500JSONResponse{
-			InternalServerErrrorJSONResponse: api.InternalServerErrrorJSONResponse{
-				Message: "Failed to create user",
-			},
-		}, nil
+		return users.CreateUser500JSONResponse{}, nil
 	}
 
-	return api.CreateUser201JSONResponse{
+	return users.CreateUser201JSONResponse{
 		UserId:    newUser.UserID.String(),
 		FirstName: newUser.FirstName,
 		LastName:  newUser.LastName,
